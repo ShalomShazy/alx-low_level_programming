6e8bfa632ac8c5971b4fe7257a724785fd334761
ShalomShazy <shazy1244@gmail.com>
2022-11-09 09:03:54 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,56 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+int is_number(char *str);
 
 /**
- * main - Prints the sum of args positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: Always zero
+ * main - Entry point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: success
  */
+
 int main(int argc, char *argv[])
 {
-	int i;
-	unsigned int k, sum = 0;
-	char *e;
+	int result = 0, i;
 
 	if (argc > 1)
 	{
-		 for (i = 1; i < argc; i++)
-		 {
-			 e = argv[i];
-
-			 for (k = 0; k < strlen(e); k++)
-			 {
-				 if (e[k] < 48 || e[k] > 57)
-				 {
-					 printf("Error\n");
-					 return (1);
-				 }
-			 }
-			 sum + = atoi(e);
-			 e++;
-		 }
-		 printf("%d\n", sum);
+		for (i = 1; i < argc; i++)
+		{
+			if (is_number(argv[i]) == 1)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				result += strtol(argv[i], NULL, 10);
+		}
+		printf("%d\n", result);
 	}
 	else
 	{
 		printf("0\n");
+		return (0);
+	}
+	exit(EXIT_SUCCESS);
+}
+/**
+ * is_number - checks if a character is a number
+ * @str: param to check
+ * Return: 0 if true else 1
+ */
+
+int is_number(char *str)
+{
+	int i = 0;
+
+	if (str[0] == '-')
+		i = 1;
+	for (; str[i]; i++)
+	{
+		if (*(str + i) < '0' || *(str + i) > '9')
+			return (1);
 	}
 	return (0);
 }
